3b2dbfeaa6ae5171674b5a34b20ead66baeba804
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-13 23:44:22 -0300

    Images styled edited

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,11 @@
 import Section from '../components/Section'
 
-const Login = () => {
+const Login = (): JSX.Element => {
     return (
         <Section className='login-section'>
             <div className='form-container'>
                 <img src='/assets/gc-logo-default.svg' alt='GoCloud Logo' />
-                <div className='image-container'>
-                    <img src='/assets/gc-robot.svg' alt='Robot Logo' />
-                </div>
+                <img src='/assets/gc-robot.svg' alt='Robot Logo' className='avatar' />
                 <div className='text-container'>
                     <p>OMNI</p>
                 </div>
