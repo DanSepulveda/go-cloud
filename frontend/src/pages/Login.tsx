097e074b20e7a02649893770d9866ae0a91704ae
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-13 23:36:01 -0300

    Images added to login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,6 +4,13 @@ const Login = () => {
     return (
         <Section className='login-section'>
             <div className='form-container'>
+                <img src='/assets/gc-logo-default.svg' alt='GoCloud Logo' />
+                <div className='image-container'>
+                    <img src='/assets/gc-robot.svg' alt='Robot Logo' />
+                </div>
+                <div className='text-container'>
+                    <p>OMNI</p>
+                </div>
             </div>
             <div className='triangle'></div>
         </Section>
