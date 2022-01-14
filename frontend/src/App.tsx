b7e15172ea777472ea957f0460945c20df3d7015
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-14 11:33:12 -0300

    Main component edited

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Main from './components/Main'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <Main>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </Main>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,22 +1,25 @@
 import Section from '../components/Section'
 import LoginForm from '../components/LoginForm'
+import Main from '../components/Main'
 
 const Login = (): JSX.Element => {
     return (
-        <Section className='login-section'>
-            <div className='data-container'>
-                <div>
-                    <img src='/assets/gc-logo-default.svg' alt='GoCloud Logo' />
-                    <img src='/assets/gc-robot.svg' alt='Robot Logo' className='avatar' />
-                    <LoginForm />
+        <Main>
+            <Section className='login-section'>
+                <div className='data-container'>
+                    <div>
+                        <img src='/assets/gc-logo-default.svg' alt='GoCloud Logo' />
+                        <img src='/assets/gc-robot.svg' alt='Robot Logo' className='avatar' />
+                        <LoginForm />
+                    </div>
+                    <div className='text-container'>
+                        <p>OMNI<span> 1.0</span></p>
+                        <p>Un servicio de <span>GoCloud</span></p>
+                    </div>
                 </div>
-                <div className='text-container'>
-                    <p>OMNI<span> 1.0</span></p>
-                    <p>Un servicio de <span>GoCloud</span></p>
-                </div>
-            </div>
-            <div className='triangle'></div>
-        </Section>
+                <div className='triangle'></div>
+            </Section>
+        </Main>
     )
 }
 
