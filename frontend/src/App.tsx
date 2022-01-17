84b1dc0cc3ea72dfca998e3d91584fe960985ace
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-17 02:49:51 -0300

    logout feature ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import './App.scss'
+import { useContext } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import UserContext from './context/UserContext'
 
 const App = () => {
+  const { userState } = useContext(UserContext)
+  const token = userState.token
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        {token && <Route path='/' element={<Dashboard />} />}
         <Route path='/login' element={<Login />} />
+        <Route path="*" element={<Navigate to={token ? '/' : "/login"} />} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { ReactComponent as Logo } from '../assets/gc-icon_logo.svg'
 import { ReactComponent as Person } from '../assets/gc-icon_logout.svg'
+import UserContext from '../context/UserContext'
+import { useContext } from 'react'
 
 const Header = (): JSX.Element => {
+    const { logout } = useContext(UserContext)
+
     return (
         <header>
             <section>
                 <Logo fill='white' />
-                <Person />
+                <Person onClick={logout} />
             </section>
             <section>
                 <span>omni</span>
