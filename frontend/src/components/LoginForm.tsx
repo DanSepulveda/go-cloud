9e05845572f7805761ef76c726a3b55a4ce2866e
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-17 02:36:44 -0300

    login feature

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,9 +1,29 @@
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
+import { useNavigate } from 'react-router-dom'
 import Input from './Input'
 import Button from './Button'
+import UserContext from '../context/UserContext'
+import { useContext } from 'react'
+import { UserProps } from '../types/contex'
 
 const LoginForm = (): JSX.Element => {
+    const navigate = useNavigate()
+    const { login } = useContext(UserContext)
+
+    const handleSubmit = async (credentials: UserProps) => {
+        try {
+            const response = await login(credentials)
+            if (response.success) {
+                navigate('/')
+                return
+            }
+            throw new Error()
+        } catch (error) {
+            alert('error')
+        }
+    }
+
     return (
         <Formik
             initialValues={{ email: '', password: '' }}
@@ -16,7 +36,7 @@ const LoginForm = (): JSX.Element => {
                     .required('Campo obligatorio')
             })}
             onSubmit={async (values, { resetForm }) => {
-                console.log(values)
+                handleSubmit(values)
             }}
         >
             <Form className='form-container'>
