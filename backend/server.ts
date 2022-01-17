a4b030f65347e1537e8cce6ccc14854586064498
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-17 11:40:20 -0300

    Ready to deploy

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,6 @@
 const express = require('express')
+import { Request, Response } from 'express'
+const path = require('path')
 import Router from './routes'
 const cors = require('cors')
 require('dotenv').config()
@@ -9,6 +11,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', Router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 4000
 
